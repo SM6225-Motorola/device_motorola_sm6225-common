595bbb7aee87a9e11cf3d405020c9bc698cd49c0
Les Lee <lesl@google.com>
2022-01-05 13:27:25 +0800

    sm6225-common: wifi: Configure correct overlay configuration.
    ag/16564596 will change default value to false.
    Configure correct overlay configuration.
    
    Bug: 212488775
    Test: manual test & build
    Change-Id: Iaf1a0a23dae69fde5995e591a757a8ef5374d5b5

--- common.mk
+++ common.mk
@@ -254,7 +254,8 @@ PRODUCT_PACKAGES += \
     SystemUIResCommon \
     TelephonyResCommon \
     TelephonyResProduct \
-    WifiResCommon
+    WifiResCommon \
+    WifiResTarget
 
 # Partitions
 PRODUCT_PACKAGES += \
